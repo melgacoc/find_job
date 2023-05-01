51a967f77650e7a31cfc7bd9b7b029ac614f98af
Cláudio <claudio19a@hotmail.com>
2023-05-01 17:26:17 -0300

    requisito 2

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -11,21 +11,8 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    data = read(path)
+    return set(unique_jobs["job_type"] for unique_jobs in data)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
