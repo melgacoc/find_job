f6cd10da4b5644488c35ca565a89405fbfd11eb6
Cláudio <claudio19a@hotmail.com>
2023-05-07 21:19:59 -0300

    requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    assert count_ocurrences(path, 'javascript') == 122
